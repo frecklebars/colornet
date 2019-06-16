2f265e098a5e9bf7e8a0cad53bd8d2837d038bf4
frecklebars <frecklebars@users.noreply.github.com>
2019-06-16 09:41:35 +0300

    updated algorithm
    Changed to what I believe to be a network with 2 input nodes, 1 hidden layer with 2 nodes and one output.
    The implementation might be wrong because it's still not working and it's all based on how I /think/ it should work.
    
    We'll see

--- BWonRGB/BWonRGB.py
+++ BWonRGB/BWonRGB.py
@@ -21,9 +21,20 @@ def findTextCol(color):
         target = 1
     return target
 
+#params
+#2 input nodes > 2 hidden nodes > 1 output node
+w11 = np.random.randn()
+w12 = np.random.randn()
+w13 = np.random.randn()
+b1 = np.random.randn()
+
+w21 = np.random.randn()
+w22 = np.random.randn()
+w23 = np.random.randn()
+b2 = np.random.randn()
+
 w1 = np.random.randn()
 w2 = np.random.randn()
-w3 = np.random.randn()
 b = np.random.randn()
 rate = 0.2
 
@@ -35,10 +46,6 @@ def sigmoid_p(x):
     x = sigmoid(x) * (1-sigmoid(x))
     return x
 
-def predict(w1, w2, w3, b, data):
-    z = w1 * data[0] + w2 * data[1] + w3 * data[2] + b
-    return z
-
 #TRAIN LOOP
 train_count = 50000
 for i in range(train_count):
@@ -47,30 +54,72 @@ for i in range(train_count):
     target = findTextCol(data)
 
     #FEED FORWARD
-    z = predict(w1, w2, w3, b, data)
+    z1 = w11 * data[0] + w12 * data[1] + w13 * data[2] + b1
+    z2 = w21 * data[0] + w22 * data[1] + w23 * data[2] + b2
+    pred1 = sigmoid(z1)
+    pred2 = sigmoid(z2)
+
+    z = pred1 * w1 + pred2 * w2 + b
     pred = sigmoid(z)
 
     #BACK PROPAGATION
     cost = (pred - target) ** 2
 
-    #calculating derivatives
+    #calculating derivatives for hidden layer
     dcost_pred = 2 * (pred - target)
     dpred_z = sigmoid_p(z)
-    dz_w1 = data[0]
-    dz_w2 = data[1]
-    dz_w3 = data[2]
+
+    dz_w1 = pred1
+    dz_w2 = pred2
     dz_b = 1
 
     dcost_w1 = dcost_pred * dpred_z * dz_w1
     dcost_w2 = dcost_pred * dpred_z * dz_w2
-    dcost_w3 = dcost_pred * dpred_z * dz_w3
     dcost_b = dcost_pred * dpred_z * dz_b
 
-    #update weights and bias
+    #update weights and bias for hidden layer
     w1 = w1 - rate * dcost_w1
     w2 = w2 - rate * dcost_w2
-    w3 = w3 - rate * dcost_w3
     b = b - rate * dcost_b
+
+    #calculating derivatives for input layer
+    dcost1_pred1 = 2 * (pred1 - target)
+    dpred1_z1 = sigmoid_p(z1)
+
+    dz1_w11 = data[0]
+    dz1_w12 = data[1]
+    dz1_w13 = data[2]
+    dz1_b1 = 1
+
+    dcost1_w11 = dcost1_pred1 * dpred1_z1 * dz1_w11
+    dcost1_w12 = dcost1_pred1 * dpred1_z1 * dz1_w12
+    dcost1_w13 = dcost1_pred1 * dpred1_z1 * dz1_w13
+    dcost1_b1 = dcost1_pred1 * dpred1_z1 * dz1_b1
+
+    dcost2_pred2 = 2 * (pred2 - target)
+    dpred2_z2 = sigmoid_p(z2)
+
+    dz2_w21 = data[0]
+    dz2_w22 = data[1]
+    dz2_w23 = data[2]
+    dz2_b2 = 1
+
+    dcost2_w21 = dcost2_pred2 * dpred2_z2 * dz2_w21
+    dcost2_w22 = dcost2_pred2 * dpred2_z2 * dz2_w22
+    dcost2_w23 = dcost2_pred2 * dpred2_z2 * dz2_w23
+    dcost2_b2 = dcost2_pred2 * dpred2_z2 * dz2_b2
+
+    #update weights and bias for hidden layer
+    w11 = w11 - rate * dcost1_w11
+    w12 = w12 - rate * dcost1_w12
+    w13 = w13 - rate * dcost1_w13
+    b1 = b1 - rate * dcost1_b1
+
+    w21 = w21 - rate * dcost2_w21
+    w22 = w22 - rate * dcost2_w22
+    w23 = w23 - rate * dcost2_w23
+    b2 = b2 - rate * dcost2_b2
+
 
 #TEST
 white = 0
@@ -78,6 +127,7 @@ black = 0
 whiteP = 0
 blackP = 0
 for i in range(100):
+    #find color and ideal text color
     color = getColor()
     textCol = findTextCol(color)
     if textCol == 1:
@@ -86,8 +136,17 @@ for i in range(100):
     else:
         textCol = "white"
         white += 1
-    z = predict(w1, w2, w3, b, color)
+
+    #predict output
+    z1 = w11 * data[0] + w12 * data[1] + w13 * data[2] + b1
+    z2 = w21 * data[0] + w22 * data[1] + w23 * data[2] + b2
+    pred1 = sigmoid(z1)
+    pred2 = sigmoid(z2)
+
+    z = pred1 * w1 + pred2 * w2 + b
     pred = sigmoid(z)
+
+    #set text color based on prediction
     if pred > 0.50:
         pred = "black"
         blackP += 1
